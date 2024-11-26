03e5676a01f1a1bae1dd72b93815ba81936faf14
Jeibii <you@example.com>
2024-11-27 04:10:52 +0800

    Deleted

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,8 @@
-
 import Sidebar from "../componets/Sidebar"
 function Dashboard() {
   return (
     <div>
+      <h1 className="text-8xl">Jayvee Brian Ibale</h1>
       <Sidebar/>
     </div>
   )
