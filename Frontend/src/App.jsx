d62c3bdb28793b821198444aa5050e930e14c2e7
Jeibii <jayveebriani@gmail.com>
2025-05-20 17:09:07 +0800

    Fix filename for case sensitivity

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -16,12 +16,12 @@ import Questionnaires from "./pages/Admin/Questionnaires";
 import Instructors from "./pages/Admin/Instructors";
 import Programs from "./pages/Admin/Programs";
 // Admin Courses
-import Bsa from "./pages/Admin/Courses/Bsa";
-import Bsais from "./pages/Admin/Courses/Bsais";
-import Bssw from "./pages/Admin/Courses/Bssw";
-import Bab from "./pages/Admin/Courses/Bab";
-import Bsis from "./pages/Admin/Courses/Bsis";
-import Act from "./pages/Admin/Courses/Act";
+import Bsa from "./pages/Admin/Courses/Bsa.jsx";
+import Bsais from "./pages/Admin/Courses/Bsais.jsx";
+import Bssw from "./pages/Admin/Courses/Bssw.jsx";
+import Bab from "./pages/Admin/Courses/Bab.jsx";
+import Bsis from "./pages/Admin/Courses/Bsis.jsx";
+import Act from "./pages/Admin/Courses/Act.jsx";
 
 import Intermediate from "./pages/Admin/Intermediate";
 import JuniorHigh from "./pages/Admin/JuniorHigh";
