f33f6a7a53a4c3ec825184026412012348913f4d
Jeibii <jayveebriani@gmail.com>
2025-05-22 23:59:28 +0800

    Cors

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://lv-tpes.vercel.app'],
+    'allowed_origins' => ['https://capstone-tpes.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
