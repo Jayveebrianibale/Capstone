71a91991022083e27fea1150932c9b53d34a3c64
Jeibii <jayveebriani@gmail.com>
2025-06-09 07:28:33 +0800

    TPES

--- Backend/app/Http/Controllers/GoogleAuthController.php
+++ Backend/app/Http/Controllers/GoogleAuthController.php
@@ -102,7 +102,7 @@ class GoogleAuthController extends Controller
 
                 // Issue token and redirect
                 $token = $user->createToken('authToken')->plainTextToken;
-                return redirect("http://localhost:5173/login?token={$token}");
+                return redirect("https://tpes.vercel.app/login?token={$token}");
 
             } catch (Exception $e) {
                 return response()->json([
